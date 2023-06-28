c46c8c2843980791e73d725d8e4ea0f259cd8bce
thatdevopsguyy <thatdevopsguyy@gmail.com>
2023-06-28 16:09:31 +0530

    added ssh keys section

--- general_scripts/2-install-dependency.sh
+++ general_scripts/2-install-dependency.sh
@@ -88,6 +88,11 @@ sudo usermod -aG docker ${USER}
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+
+# Ssh keys creation 
+echo "Creating ssh keys for private repos..."
+ssh-keygen -t rsa -b 4096 -N ""
+
 # Terraform user account auth 
 az login --use-device-code 
 
